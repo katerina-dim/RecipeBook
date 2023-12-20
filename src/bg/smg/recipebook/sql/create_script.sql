35bc95657e4681ffd0399f9d75cf324853ba7522
Katerina Dimitrova <kateto_dim@abv.bg>
2023-12-20 17:31:35 +0200

    Initial commit

---
+++ src/bg/smg/recipebook/sql/create_script.sql
@@ -0,0 +1,9 @@
+create database recipebook;
+
+create table users (
+id int not null primary key auto_increment,
+username varchar(32),
+password varchar(32),
+created timestamp,
+is_active boolean
+);
